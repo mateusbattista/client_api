0b1a28300432dbb8446f3b3036e5c531dd01336c
Mateus Batista <batista.mateus182@gmail.com>
2020-12-14 21:02:51 -0300

    Boas práticas feitas no urls.py e correção no apontamento pro db no settings

--- api_client/settings.py
+++ api_client/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'api_db',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
